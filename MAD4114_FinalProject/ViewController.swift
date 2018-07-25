a3de9f1b57ceef96db97996887b20844090cbc73
Shiva Kavya <shivakavya@shivas-macbook-pro.local>
2018-07-25 14:14:42 -0400

    Initial Commit

---
+++ MAD4114_FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAD4114_FinalProject
+//
+//  Created by Shiva Kavya on 2018-07-25.
+//  Copyright © 2018 Shiva Kavya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
